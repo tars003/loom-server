829b741ae25b83e00f89f3fe6681ba9385d0017e
tars003 <sharmaajay2kuu@gmail.com>
2021-11-10 21:28:00 +0530

    added mqtt

--- server.js
+++ server.js
@@ -6,17 +6,28 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: '*' }, allowEIO3: true });
 const moment = require('moment');
-var mqtt    = require('mqtt');
+var mqtt = require('mqtt');
 
 // MQTT CLIENT CONFIG
-var client  = mqtt.connect("mqtt://test.mosquitto.org",{clientId:"mqttjs01"});
+const host = '192.168.1.23'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-app.use(cors());
-app.use(express.json());
+const connectUrl = `mqtt://${host}:${port}`
+const mainTopic = '/nodejs/mqtt'
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'emqx',
+    password: 'public',
+    reconnectPeriod: 1000,
+});
 
 const RunningShift = require('./models/RunningShift.model');
 const Shift = require('./models/Shift.model');
 const Employee = require('./models/Employee.model');
+const map = require('./utils/constants');
 
 const { reportLiveStatus } = require('./utils/esp32.util');
 const { initialConnection, updateDashboard } = require('./utils/dashboard.util');
@@ -24,27 +35,25 @@ const { init, update } = require('./models/RunningShift.model');
 
 
 const connectDB = require('./utils/db');
+const e = require('express');
 connectDB();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-app.post('/report-live-status', async(req, res) => {
+app.post('/report-live-status', async (req, res) => {
     try {
-        const data  = req.body;
+        const data = req.body;
         console.log(data);
-        // updateDash(data);
         const runningShift = await reportLiveStatus(data);
-        // console.log();
-        // console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-        // console.log(runningShift);
         updateDash(runningShift);
         return res.status(200).json({
             success: true,
-            // data : runningShift
         });
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(503).json({
             success: false
@@ -52,34 +61,53 @@ app.post('/report-live-status', async(req, res) => {
     }
 });
 
-// MQTT CONFIG
-client.on("connect",function(){	
-    console.log("connected");
+// // MQTT CONFIG
+client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe([mainTopic], () => {
+        console.log(`Subscribe to topic '${topic}'`)
+    })
+})
+client.on('message', (topic, payload) => {
+    let inStr = payload.toString().replaceAll("'", "\"");
+    console.log('Received Message:', topic, JSON.parse(inStr));
+
+    if(topic == mainTopic) {
+        const { time, stationId, tags } = JSON.parse(inStr);
+        let tag = tags.map(tag => {
+            if(tag.tagId == map[stationId]) return tag;
+        });
+        tag = tag[0];
+        const { tagId, rssi } = tag;
+        let detected;
+        if(Math.abs(rssi) < map['threshold']) detected = true;
+        else false;
+        // const newTime = 
+
+        const data = {
+            dateTimeString : time,
+            stationId : stationId,
+            tagId : tagId,
+            detected : detected,
+            rssi: rssi
+        }
+
+        const runningShift = await reportLiveStatus(data);
+        updateDash(runningShift);
+    }
 });
 
-client.on("error",function(error){ console.log("Can't connect"+error)});
+// UPDATES DASH WITH EMPLOYEE DATA INCOMING FROM ESP32
+const updateDash = (runningShift) => {
+    io.emit('dashboard-update', { 'runningShift': runningShift });
+}
 
-client.subscribe(topic, [options], [callback]);
-
-client.on('message',function(topic, message, packet){
-	console.log("message is "+ message);
-	console.log("topic is "+ topic);
-    
-    // input data
-    // data = {
-    //     stationId: 'STATION1',
-    //     tagId: 'nfklvks72y394inl',
-    //     dateTime: '12-09-21 21:45:10',
-    //     detected: true,
-    //     rssi: 90,
-    // }
-    const empData = await reportLiveStatus(message);
-    updateDash(empData);
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening on *:3000');
 });
 
 
 // SOCKET CONFIG
-
 io.on('connection', async (socket) => {
     console.log('A station connected');
 
@@ -98,6 +126,7 @@ io.on('connection', async (socket) => {
         const empData = reportLiveStatus(data);
     });
 
+
     // event name -> initial-connection-dashboard
     // data = {
     //     clientId: 'KJVKLBLSK89'
@@ -109,7 +138,7 @@ io.on('connection', async (socket) => {
 
         const runningShift = await initialConnection(data);
 
-        io.to(data.clientId).emit('running-shift-data', {'runningShift' : runningShift});
+        io.to(data.clientId).emit('running-shift-data', { 'runningShift': runningShift });
     });
 
 
@@ -124,11 +153,17 @@ io.on('connection', async (socket) => {
 
 });
 
-// UPDATES DASH WITH EMPLOYEE DATA INCOMING FROM ESP32
-const updateDash = (runningShift) => {
-    io.emit('dashboard-update', {'runningShift' : runningShift});
-}
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log('listening on *:3000');
-});
+// {
+//     "stationId" : "STATION1",
+//     "time": "12-11-2021-11:24:12",
+//     "tags": [
+//         {
+//             "tagId": "TAG 1",
+//             "rssi": 81
+//         },
+//         {
+//             "tagId": "TAG 2",
+//             "rssi": 40
+//         },
+//     ]
+// }
---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+const map = {
+    'threshold' : 80,
+    'STATION1': 'TAG1',
+    'STATION2': 'TAG2',
+    'STATION3': 'TAG3',
+    'STATION4': 'TAG4',
+    'STATION5': 'TAG5',
+    'STATION6': 'TAG6',
+    'STATION7': 'TAG7',
+    'STATION8': 'TAG8',
+    'STATION9': 'TAG9',
+    'STATION10': 'TAG10',
+}
+
+module.exports = map;
